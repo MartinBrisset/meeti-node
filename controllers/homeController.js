08e8ed8cea0e4c16015878f2877ea4711954d01b
martin <brissetmartin@gmail.com>
2020-05-20 11:54:16 -0300

    agregando datos al front

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,16 @@
-exports.home = (req, res) => {
+const Categorias = require('../models/Categorias')
+
+exports.home = async (req, res) => {
+
+    //crear promesa para hacer muchas consultas al mismo tiempo ya que no tienen que esperarse entre ellas por no estar relacionadas
+    const consultas = []
+    consultas.push(Categorias.findAll({}))
+
+    //extraer y pasar a la vista
+    const [categorias] = await Promise.all(consultas)
+
     res.render('home', {
-        nombrePagina: 'Inicio'
+        nombrePagina: 'Inicio',
+        categorias
     })
 }
